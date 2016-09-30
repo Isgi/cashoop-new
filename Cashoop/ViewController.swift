781bc56f39742de85b320e9db61ae01a0a3304ec
Isgi Arriza <isgiarriza@gmail.com>
2016-10-01 02:06:28 +0700

    Initial Commit

---
+++ Cashoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cashoop
+//
+//  Created by Isgi Arriza on 10/1/16.
+//  Copyright © 2016 Isgi Arriza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
